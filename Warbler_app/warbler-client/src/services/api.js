ec5eaa929049b2aaf649726d7273226567203701
HINA KHADIM <hinakhadim2002@gmail.com>
2020-03-30 19:15:23 +0500

    Login complete :smiley:

---
+++ Warbler_app/warbler-client/src/services/api.js
@@ -0,0 +1,27 @@
+// import axios from "axios";
+
+// export function apiCall(method,path,data){
+//     return new Promise((resolve,reject)=>{
+//      return axios[method](path,data)
+//          .then(res=>{
+//          return resolve(res.data);
+//          })
+//          .catch(err=>{
+//          return reject(err.response.data.error);
+//      })
+// });
+// }
+
+import axios from "axios";
+
+export function apiCall(method, path, data) {
+  return new Promise((resolve, reject) => {
+    return axios[method](path, data)
+      .then(res => {
+        return resolve(res.data);
+      })
+      .catch(err => {
+        return reject(err.response.data.error);
+      });
+  });
+}
